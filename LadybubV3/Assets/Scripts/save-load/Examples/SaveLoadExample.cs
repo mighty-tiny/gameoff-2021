cd2aaf69e31d756012cd1510c953e8ecb6fae0ab
fuwwyUwU <vsjoqvist@gmail.com>
2021-11-03 17:01:13 +0100

    added more comments

--- LadybubV3/Assets/Scripts/save-load/Examples/SaveLoadExample.cs
+++ LadybubV3/Assets/Scripts/save-load/Examples/SaveLoadExample.cs
@@ -7,12 +7,14 @@ using System;
 //make sure to include inherit from the ISaveable interface. Its in the Ladybug.SaveSystem namespace. Look at line 4
 public class SaveLoadExample : MonoBehaviour, ISaveable
 {
+    //the Gameobjects value
     [SerializeField] private int score = 10;
+    //save the data
     public object CaptureState()
     {
         return new saveData { score = score, xPos = transform.position.x, yPos = transform.position.y, zPos = transform.position.z };
     }
-
+    //load the data
     public void RestoreState(object state)
     {
         var saveData = (saveData)state;
@@ -21,6 +23,7 @@ public class SaveLoadExample : MonoBehaviour, ISaveable
         transform.position = new Vector3(saveData.xPos, saveData.yPos, saveData.zPos);
     }
 
+    //the save data
     [Serializable]
     private struct saveData
     {
