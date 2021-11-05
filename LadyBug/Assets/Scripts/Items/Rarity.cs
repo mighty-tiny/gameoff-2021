6c667642f463d8494cd517a107c1dbdace5dcc42
fuwwyUwU <vsjoqvist@gmail.com>
2021-11-05 15:36:01 +0100

    basic shop system implemented

---
+++ LadyBug/Assets/Scripts/Items/Rarity.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Rarity", menuName = "Rarity")]
+public class Rarity : ScriptableObject
+{
+
+    [SerializeField] private Color rarityColor;
+    [SerializeField] private string rarityName;
+
+    public Color RarityColor => rarityColor;
+    public string RarityName => rarityName;
+}
