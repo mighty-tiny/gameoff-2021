d35447a5c1b4aa0e6c8b14953f186bfdd55261a3
fuwwyUwU <vsjoqvist@gmail.com>
2021-11-09 21:20:14 +0100

    started working on adjusting the inventory
    NOT DONE YET

---
+++ LadyBugV3/Assets/Scripts/Inventory/CollectableItemObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Collectable Item", menuName = "Inventory System/Item/Collectable Item")]
+public class CollectableItemObject : ItemObject
+{
+    //
+}
---
+++ LadyBugV3/Assets/Scripts/Inventory/PlayerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public InventoryScript inventory;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("COLLIDED");
+        var item = other.GetComponent<Item>();
+        if (item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
+}
